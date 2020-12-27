c3ce9e2e5ff1dd7ba4e73f9a0bd46abaa82349bf
Surya Karmakar <surjokarmakarssj7@gmail.com>
2020-12-27 21:27:41 +0530

    rebuild structure, version 1.1

--- macChanger.py
+++ macChanger.py
@@ -1,7 +1,15 @@
 import subprocess
 
-subprocess.call("ifconfig eth0",shell=True)
-subprocess.call("ifconfig eth0 down",shell=True)
-subprocess.call("ifconfig eth0 hw ether 00:11:22:33:44:55",shell=True)
-subprocess.call("ifconfig eth0 up",shell=True)
-subprocess.call("ifconfig eth0",shell=True)
+def mac_Changer(interface, newMac):
+    subprocess.call(["ifconfig",interface])
+    subprocess.call(["ifconfig",interface,"down"])
+    subprocess.call(["ifconfig",interface,"hw","ether",newMac])
+    subprocess.call(["ifconfig",interface,"up"])
+    subprocess.call(["ifconfig",interface])
+
+def main():
+    interface = input("[+]Enter your interface name : ")
+    newMac = input("[+]Enter your new mac address : ")
+    mac_Changer(interface, newMac)
+if __name__ == "__main__":
+    main()
